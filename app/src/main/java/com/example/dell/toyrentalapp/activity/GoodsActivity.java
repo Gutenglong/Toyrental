ffd5bd46a1dd8d9fe7ae127fad38727c91193966
grang001 <893509042@qq.com>
2018-03-13 19:37:37 +0800

    商品详情页

---
+++ app/src/main/java/com/example/dell/toyrentalapp/activity/GoodsActivity.java
@@ -0,0 +1,18 @@
+package com.example.dell.toyrentalapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.dell.toyrentalapp.R;
+
+/**
+ * Created by Administrator on 2018/3/13 0013.
+ */
+
+public class GoodsActivity  extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods);
+    }
+}
